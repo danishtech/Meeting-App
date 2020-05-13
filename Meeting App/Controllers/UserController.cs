000c30bc484316da88ee7eb08acb51edb2c75f10
Danish Ahmad <danish.tech@hotmail.com>
2020-05-13 15:36:55 +0530

    mail changes

--- Meeting App/Controllers/UserController.cs
+++ Meeting App/Controllers/UserController.cs
@@ -29,6 +29,56 @@ namespace Meeting_App.Controllers
             return db.AppUsers;
         }
         [HttpPost]
+        [Route("api/User/SendAgendaEmail")]
+        public async Task SendAgendaEmail([FromBody]JObject objData)
+        {
+            var message = new MailMessage();
+
+            string[] Multi = objData["toemail"].ToString().Split(','); //spiliting input Email id string with comma(,)
+            string[] Multiarray = objData["toname"].ToString().Split(',');
+            message.From = new MailAddress("checkboxnoida@gmail.com");
+            message.Bcc.Add(new MailAddress("checkboxnoida@gmail.com"));
+            message.Subject = objData["subject"].ToString();
+
+            //adding multi reciver's Email Id
+
+            //spiliting input Email id string with comma(,)
+            foreach (string Multiname in Multiarray)
+            {
+                foreach (string Multiemailid in Multi)
+                {
+                    message.To.Add(new MailAddress(Multiemailid)); //  message.To.Add(new MailAddress(Multiname)); //adding multi reciver's Email Id
+                    message.Body = createAgendaEmailBody(Multiname, objData["message"].ToString(), objData["MeetingSubject"].ToString(), objData["MeetingDate"].ToString(), objData["HostUser"].ToString(), objData["ShareLink"].ToString(), objData["MeetingDescription"].ToString(),objData["NewMeetingDescription"].ToString());
+                    message.IsBodyHtml = true;
+                    using (var smtp = new SmtpClient())
+                    {
+                        await smtp.SendMailAsync(message);
+                        await Task.FromResult(0);
+                    }
+
+                }
+
+            }
+
+        }
+        private string createAgendaEmailBody(string userName, string message, string subject, string MeetingDate, string HostUser, string ShareLink, string MeetingDescription, string NewMeetingDescription)
+        {
+            string body = string.Empty;
+            using (StreamReader reader = new StreamReader(HttpContext.Current.Server.MapPath("/Meeting_Agenda_Changed.html")))
+            {
+                body = reader.ReadToEnd();
+            }
+            body = body.Replace("{{UserName}}", userName);
+            body = body.Replace("{{message}}", message);
+            body = body.Replace("{{MeetingDate}}", MeetingDate);
+            body = body.Replace("{{MeetingSubject}}", subject);
+            body = body.Replace("{{HostUser}}", HostUser);
+            body = body.Replace("{{ShareLink}}", ShareLink);
+            body = body.Replace("{{meetingdescription}}", MeetingDescription);
+            body = body.Replace("{{Newmeetingdescription}}", NewMeetingDescription);
+            return body;
+        }
+        [HttpPost]
         [Route("api/User/SendEmail")]
         public async Task SendEmail([FromBody]JObject objData)
         {
@@ -39,40 +89,194 @@ namespace Meeting_App.Controllers
             message.From = new MailAddress("checkboxnoida@gmail.com");
             message.Bcc.Add(new MailAddress("checkboxnoida@gmail.com"));
             message.Subject = objData["subject"].ToString();
-            
-                //adding multi reciver's Email Id
-                                                            
-                //spiliting input Email id string with comma(,)
-                foreach (string Multiname in Multiarray)
-                {
+
+            //adding multi reciver's Email Id
+
+            //spiliting input Email id string with comma(,)
+            foreach (string Multiname in Multiarray)
+            {
                 foreach (string Multiemailid in Multi)
                 {
                     message.To.Add(new MailAddress(Multiemailid)); //  message.To.Add(new MailAddress(Multiname)); //adding multi reciver's Email Id
-                    message.Body = createEmailBody(Multiname, objData["message"].ToString());
+                    message.Body = createEmailBody(Multiname, objData["message"].ToString(), objData["MeetingSubject"].ToString(), objData["MeetingDate"].ToString(), objData["HostUser"].ToString(), objData["ShareLink"].ToString(), objData["MeetingDescription"].ToString());
                     message.IsBodyHtml = true;
                     using (var smtp = new SmtpClient())
                     {
                         await smtp.SendMailAsync(message);
                         await Task.FromResult(0);
                     }
-                
+
                 }
 
             }
-            
+
         }
-        private string createEmailBody(string userName, string message)
+        private string createEmailBody(string userName, string message, string subject, string MeetingDate, string HostUser, string ShareLink, string MeetingDescription)
         {
             string body = string.Empty;
-            using (StreamReader reader = new StreamReader(HttpContext.Current.Server.MapPath("/htmlTemplate.html")))
+            using (StreamReader reader = new StreamReader(HttpContext.Current.Server.MapPath("/Meeting_Invitation.html")))
             {
                 body = reader.ReadToEnd();
             }
-            body = body.Replace("{UserName}", userName);
-            body = body.Replace("{message}", message);
+            body = body.Replace("{{UserName}}", userName);
+            body = body.Replace("{{message}}", message);
+            body = body.Replace("{{MeetingDate}}", MeetingDate);
+            body = body.Replace("{{MeetingSubject}}", subject);
+            body = body.Replace("{{HostUser}}", HostUser);
+            body = body.Replace("{{ShareLink}}", ShareLink);
+            body = body.Replace("{{meetingdescription}}", MeetingDescription);
+            return body;
+        }
+    
+        [HttpPost]
+        [Route("api/User/SendResheduleMeetingEmail")]
+        public async Task SendResheduleMeetingEmail([FromBody]JObject objData)
+        {
+            var message = new MailMessage();
+
+            string[] Multi = objData["toemail"].ToString().Split(','); //spiliting input Email id string with comma(,)
+            string[] Multiarray = objData["toname"].ToString().Split(',');
+            message.From = new MailAddress("checkboxnoida@gmail.com");
+            message.Bcc.Add(new MailAddress("checkboxnoida@gmail.com"));
+            message.Subject = objData["subject"].ToString();
+
+            //adding multi reciver's Email Id
+
+            //spiliting input Email id string with comma(,)
+            foreach (string Multiname in Multiarray)
+            {
+                foreach (string Multiemailid in Multi)
+                {
+                    message.To.Add(new MailAddress(Multiemailid)); //  message.To.Add(new MailAddress(Multiname)); //adding multi reciver's Email Id
+                    message.Body = SendResheduleMeetingEmailBody(Multiname, objData["message"].ToString(), objData["MeetingSubject"].ToString(), objData["MeetingDate"].ToString(), objData["HostUser"].ToString(), objData["ShareLink"].ToString(), objData["MeetingDescription"].ToString(), objData["NewMeetingDate"].ToString());
+                    message.IsBodyHtml = true;
+                    using (var smtp = new SmtpClient())
+                    {
+                        await smtp.SendMailAsync(message);
+                        await Task.FromResult(0);
+                    }
+
+                }
+
+            }
+
+        }
+        private string SendResheduleMeetingEmailBody(string userName, string message, string subject, string MeetingDate, string HostUser, string ShareLink, string MeetingDescription,string NewMeetingDate)
+        {
+            string body = string.Empty;
+            using (StreamReader reader = new StreamReader(HttpContext.Current.Server.MapPath("/Meeting_Reschedule.html")))
+            {
+                body = reader.ReadToEnd();
+            }
+            body = body.Replace("{{UserName}}", userName);
+            body = body.Replace("{{message}}", message);
+            body = body.Replace("{{MeetingDate}}", MeetingDate);
+            body = body.Replace("{{MeetingSubject}}", subject);
+            body = body.Replace("{{HostUser}}", HostUser);
+            body = body.Replace("{{ShareLink}}", ShareLink);
+            body = body.Replace("{{meetingdescription}}", MeetingDescription);
+            body = body.Replace("{{NewMeetingDate}}", NewMeetingDate);
+            return body;
+        }
+        [HttpPost]
+        [Route("api/User/SendCancellationMeetingEmail")]
+        public async Task SendCancellationMeetingEmail([FromBody]JObject objData)
+        {
+            var message = new MailMessage();
+
+            string[] Multi = objData["toemail"].ToString().Split(','); //spiliting input Email id string with comma(,)
+            string[] Multiarray = objData["toname"].ToString().Split(',');
+            message.From = new MailAddress("checkboxnoida@gmail.com");
+            message.Bcc.Add(new MailAddress("checkboxnoida@gmail.com"));
+            message.Subject = objData["subject"].ToString();
+
+            //adding multi reciver's Email Id
+
+            //spiliting input Email id string with comma(,)
+            foreach (string Multiname in Multiarray)
+            {
+                foreach (string Multiemailid in Multi)
+                {
+                    message.To.Add(new MailAddress(Multiemailid)); //  message.To.Add(new MailAddress(Multiname)); //adding multi reciver's Email Id
+                    message.Body = SendCancellationMeetingEmailBody(Multiname, objData["message"].ToString(), objData["MeetingSubject"].ToString(), objData["MeetingDate"].ToString(), objData["HostUser"].ToString(), objData["ShareLink"].ToString(), objData["MeetingDescription"].ToString(), objData["NewMeetingDate"].ToString());
+                    message.IsBodyHtml = true;
+                    using (var smtp = new SmtpClient())
+                    {
+                        await smtp.SendMailAsync(message);
+                        await Task.FromResult(0);
+                    }
+
+                }
+
+            }
+
+        }
+        private string SendCancellationMeetingEmailBody(string userName, string message, string subject, string MeetingDate, string HostUser, string ShareLink, string MeetingDescription, string NewMeetingDate)
+        {
+            string body = string.Empty;
+            using (StreamReader reader = new StreamReader(HttpContext.Current.Server.MapPath("/Meeting_cancellation.html")))
+            {
+                body = reader.ReadToEnd();
+            }
+            body = body.Replace("{{UserName}}", userName);
+            body = body.Replace("{{message}}", message);
+            body = body.Replace("{{MeetingDate}}", MeetingDate);
+            body = body.Replace("{{MeetingSubject}}", subject);
+            body = body.Replace("{{HostUser}}", HostUser);
+            body = body.Replace("{{ShareLink}}", ShareLink);
+            body = body.Replace("{{meetingdescription}}", MeetingDescription);
+            body = body.Replace("{{NewMeetingDate}}", NewMeetingDate);
             return body;
         }
 
+        [HttpPost]
+        [Route("api/User/SendConclusionMeetingEmailBody")]
+        public async Task SendConclusionMeetingEmailBody([FromBody]JObject objData)
+        {
+            var message = new MailMessage();
+
+            string[] Multi = objData["toemail"].ToString().Split(','); //spiliting input Email id string with comma(,)
+            string[] Multiarray = objData["toname"].ToString().Split(',');
+            message.From = new MailAddress("checkboxnoida@gmail.com");
+            message.Bcc.Add(new MailAddress("checkboxnoida@gmail.com"));
+            message.Subject = objData["subject"].ToString();
+
+            //adding multi reciver's Email Id
+
+            //spiliting input Email id string with comma(,)
+            foreach (string Multiname in Multiarray)
+            {
+                foreach (string Multiemailid in Multi)
+                {
+                    message.To.Add(new MailAddress(Multiemailid)); //  message.To.Add(new MailAddress(Multiname)); //adding multi reciver's Email Id
+                    message.Body = SendConclusionMeetingEmailBody(Multiname, objData["message"].ToString(), objData["MeetingSubject"].ToString(), objData["HostUser"].ToString(), objData["MeetingDescription"].ToString(), objData["Conclusion"].ToString());
+                    message.IsBodyHtml = true;
+                    using (var smtp = new SmtpClient())
+                    {
+                        await smtp.SendMailAsync(message);
+                        await Task.FromResult(0);
+                    }
+
+                }
+
+            }
+
+        }
+        private string SendConclusionMeetingEmailBody(string userName, string message, string subject, string HostUser, string MeetingDescription, string Conclusion)
+        {
+            string body = string.Empty;
+            using (StreamReader reader = new StreamReader(HttpContext.Current.Server.MapPath("/Meeting_Conclusion.html")))
+            {
+                body = reader.ReadToEnd();
+            }
+            body = body.Replace("{{UserName}}", userName);
+            body = body.Replace("{{message}}", message);
+            body = body.Replace("{{MeetingSubject}}", subject);
+            body = body.Replace("{{HostUser}}", HostUser);
+            body = body.Replace("{{meetingdescription}}", MeetingDescription);
+            body = body.Replace("{{Conclusion}}", Conclusion);
+            return body;
+        }
         [HttpPost]
         [Route("api/User/ForgetPassword")]
         public async Task ForgetPassword([FromBody]JObject objData)
@@ -102,11 +306,11 @@ namespace Meeting_App.Controllers
             return body;
         }
         [HttpGet]
-       [Route("api/User/UserExists")]
+        [Route("api/User/UserExists")]
         public bool UserExists(string id)
         {
             // string email = Convert.ToString(id);
-            bool userAlreadyExists = db.AppUsers.Any(x => x.LoginName == id);            
+            bool userAlreadyExists = db.AppUsers.Any(x => x.LoginName == id);
 
             return userAlreadyExists;
         }
@@ -124,9 +328,9 @@ namespace Meeting_App.Controllers
         public bool UserExists(string Loginname, string password)
         {
             // string email = Convert.ToString(id);
-            bool userExists = db.AppUsers.Any(x => x.LoginName == Loginname && x.Password==password);
+            bool userExists = db.AppUsers.Any(x => x.LoginName == Loginname && x.Password == password);
 
-            return userExists; 
+            return userExists;
         }
         // GET: api/User/5
         [ResponseType(typeof(AppUser))]
